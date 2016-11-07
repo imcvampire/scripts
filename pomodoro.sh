9f1b6818eaa0a3ddd155b2059145b6b9b993d0cf
imcvampire <quocanh96@gmail.com>
2016-11-07 21:43:15 +0700

    feat: .

---
+++ pomodoro.sh
@@ -0,0 +1,8 @@
+sleep 2700 && notify-send "break"
+sleep 300 && notify-send "back to work"
+sleep 2700 && notify-send "break"
+sleep 300 && notify-send "back to work"
+sleep 2700 && notify-send "break"
+sleep 300 && notify-send "back to work"
+sleep 2700 && notify-send "break"
+sleep 900 && notify-send "back to work"
